19a53fec8b8374e4ab7a5110321b691a455ed06c
Yves Brissaud <yves@dagger.io>
2026-04-09 09:20:54 +0200

    sdk/groovy: add Groovy annotation definitions
    Signed-off-by: Yves Brissaud <yves@dagger.io>

---
+++ sdk/groovy/dagger-groovy-sdk/src/main/groovy/io/dagger/groovy/annotation/DefaultPath.groovy
@@ -0,0 +1,12 @@
+package io.dagger.groovy.annotation
+
+import java.lang.annotation.ElementType
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import java.lang.annotation.Target
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.SOURCE)
+@interface DefaultPath {
+    String value()
+}
